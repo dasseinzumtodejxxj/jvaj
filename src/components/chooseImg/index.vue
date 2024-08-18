2b21c41a88f0538f5617a640a9ea6cb9a658ee7a
dasseinzumtodejxxj <15585739550@163.com>
2024-08-18 18:44:33 +0800

    first commit

---
+++ src/components/chooseImg/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-drawer v-model="drawer" title="媒体库" size="650px">
+    <warning-bar
+        title="点击“文件名/备注”可以编辑文件名或者备注内容。"
+    />
+  </el-drawer>
+</template>
+<script setup>
+
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
